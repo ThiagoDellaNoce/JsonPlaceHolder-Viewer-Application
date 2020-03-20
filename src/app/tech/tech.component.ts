429625d26d2e8d088705caa08fd62738e87f1081
Thiago Della Noce <thiagodellanoce.ingressolive@outlook.com>
2020-03-20 01:42:08 -0300

    Melhoria no UX - retirada do 'call to action'

--- src/app/tech/tech.component.ts
+++ src/app/tech/tech.component.ts
@@ -12,7 +12,6 @@ export class TechComponent implements OnInit {
   constructor(private router: Router) {};
 
   ngOnInit() {
-
     switch (this.router.url) {
       case "/":
         this.changeTitle("Home");
